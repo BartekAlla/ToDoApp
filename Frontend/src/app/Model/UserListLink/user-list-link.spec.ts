44ae432d2743d75202a33747d4195010d442ee18
Bartlomiej <b.allaburda@gmail.com>
2024-02-05 19:34:41 +0100

    getting user's lists by angular

---
+++ Frontend/src/app/Model/UserListLink/user-list-link.spec.ts
@@ -0,0 +1,7 @@
+import { UserListLink } from './user-list-link';
+
+describe('UserListLink', () => {
+  it('should create an instance', () => {
+    expect(new UserListLink()).toBeTruthy();
+  });
+});
---
+++ Frontend/src/app/Model/UserListLink/user-list-link.ts
@@ -0,0 +1,15 @@
+import {UserInfo} from "../User/userInfo";
+import {UserList} from "../UserList/user-list";
+
+export class UserListLink {
+  userInfo: UserInfo;
+  userList: UserList;
+  listRole: string;
+  listName: string;
+  constructor() {
+    this.userInfo = new UserInfo();
+    this.userList = new UserList();
+    this.listRole = '';
+    this.listName = '';
+  }
+}
